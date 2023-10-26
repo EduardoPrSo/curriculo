e4df455da67ca925608a7604f490343f405f2af6
DuDuPrSo <85527054+EduardoPrSo@users.noreply.github.com>
2023-10-26 08:25:15 -0300

    Edit config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,7 @@
 import { Eduardo } from "./configEduardo.js";
-import { Daisa } from "./configDaisa.js";
 
 const profileConfig = {}
 
 profileConfig["Eduardo"] = Eduardo;
-profileConfig["Daisa"] = Daisa;
 
 export { profileConfig };
